dc2a6ebda95f5705458447b092521d184b905445
seyamasan <kst_apr_24@icloud.com>
2025-12-08 19:20:00 +0900

    add: Lesson1Viewを追加

---
+++ SwiftKindergarten/Lesson1/Lesson1View.swift
@@ -0,0 +1,21 @@
+//
+//  Lesson1View.swift
+//  SwiftKindergarten
+//
+//  Created by 瀬山皐太 on 2025/12/08.
+//
+
+import SwiftUI
+
+struct Lesson1View: View {
+    var body: some View {
+        
+        Text("Lesson1")
+        
+        .navigationTitle("Lesson1")
+    }
+}
+
+#Preview {
+    Lesson1View()
+}
