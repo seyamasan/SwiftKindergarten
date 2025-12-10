4f3628331dbb3605050abf97ccdbb56ce14b8f42
seyamasan <kst_apr_24@icloud.com>
2025-12-10 19:39:14 +0900

    feat: ボタンをタップでViewが更新されるように実装

--- SwiftKindergarten/Lesson1/Lesson1View.swift
+++ SwiftKindergarten/Lesson1/Lesson1View.swift
@@ -9,9 +9,15 @@ import SwiftUI
 
 struct Lesson1View: View {
     @State private var lesson1 = Lesson1()
+    @State private var count = 0
     
     var body: some View {
-        Text(self.lesson1.constText1)
+        VStack(spacing: 32) {
+            Text("\(self.count)回変更された\(self.lesson1.getVarText2())")
+            Button("変更する") {
+                self.count += 1
+            }
+        }
         .navigationTitle("Lesson1")
     }
 }
