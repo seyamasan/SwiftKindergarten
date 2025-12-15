7b60af7354aef4d9694a5bcc41bbbd874f580ebb
seyamasan <kst_apr_24@icloud.com>
2025-12-15 19:19:26 +0900

    fix: ObservationをCombineに変更

--- SwiftKindergarten/Lesson1/Lesson1.swift
+++ SwiftKindergarten/Lesson1/Lesson1.swift
@@ -6,18 +6,17 @@
 //
 //  初心者向けに作っているため、あまり言語に依存しないような書き方にしています。
 
-import Observation
+import Combine
 
-@Observable
-class Lesson1 {
+class Lesson1: ObservableObject {
     // 修飾子をつけない場合はinternal
     // 同一モジュール内なら参照可能
-    @ObservationIgnored let constText1: String = "メンバ定数" // 定数なので再代入できない
-    var varText1: String = "メンバ変数" // 変数なので再代入できる
+    let constText1: String = "メンバ定数" // 定数なので再代入できない
+    @Published var varText1: String = "メンバ変数" // 変数なので再代入できる
     
     // privateはこのクラスからのみ参照可能
-    @ObservationIgnored private let constText2: String = "プライベートメンバ定数"
-    private var count: Int = 0
+    private let constText2: String = "プライベートメンバ定数"
+    @Published private var count: Int = 0
     
     func getConstText2() -> String {
         return self.constText2
