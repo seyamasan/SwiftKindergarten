4633b31abd8f62171f3ab1cad4aa09673bbb5622
seyamasan <kst_apr_24@icloud.com>
2026-01-20 23:46:52 +0900

    feat: Intの最大値を出力する関数を実装

--- SwiftKindergarten/Lesson1/Lesson1_2/Lesson1_2.swift
+++ SwiftKindergarten/Lesson1/Lesson1_2/Lesson1_2.swift
@@ -25,4 +25,25 @@ class Lesson1_2 {
             print("Shift JISでエンコードした文字列: \(broken)")
         }
     }
+    
+    func showMaxAndMinInt() {
+        // uname -m をターミナルで実行すると確認できる
+        // 作者のMacのプロセッサは、x86_64で64bitなので
+        let maxInt64: Int = 9223372036854775807
+        let minInt64: Int = -9223372036854775808
+        print("Intの最大値: \(maxInt64), 最小値: \(minInt64)")
+        
+        // オーバーフロー 64bit
+//        let overflowMaxInt64: Int = 9223372036854775808
+//        let overflowMinInt64: Int = -9223372036854775809
+        
+        // 32bitの場合
+//        let maxInt32: Int = 2147483647
+//        let minInt32: Int = -2147483648
+//        print("Intの最大値: \(maxInt32), 最小値: \(minInt32)")
+        
+        // オーバーフロー 32bit
+//        let overflowMaxInt32: Int = 2147483648
+//        let overflowMinInt32: Int = -2147483649
+    }
 }
